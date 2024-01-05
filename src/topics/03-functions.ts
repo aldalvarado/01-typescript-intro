9fca1e23478820e41bbda2bd83baf33047117c12
aldalvarado <aaldoandre@gmail.com>
2024-01-05 16:54:30 -0600

    Primera Tarea

--- src/topics/03-functions.ts
+++ src/topics/03-functions.ts
@@ -1,39 +1,39 @@
-function addNumbers(a: number, b: number): number {
-    return a+b;
-}
+//function addNumbers(a: number, b: number): number {
+ //   return a+b;
+//}
 
-const addNumbersArrow = (a: number, b: number): string => {
-    return `${ a + b }`; 
-}
+//const addNumbersArrow = (a: number, b: number): string => {
+ //   return `${ a + b }`; 
+//}
 
-function Multiply (FirstNumber: number, SecondNumber?: number, Base: number= 2) {
-    return FirstNumber * Base;
-}
+//function Multiply (FirstNumber: number, SecondNumber?: number, Base: number= 2) {
+ //   return FirstNumber * Base;
+//}
 
-interface Character {
-    name: string;
-    hp: number;
-    showHP: () => void;
-}
+//interface Character {
+ //   name: string;
+ //   hp: number;
+ //   showHP: () => void;
+//}
 
-const healCharacter = ( character: Character, amount: number )=> {
-    character.hp += amount;
-}
+//const healCharacter = ( character: Character, amount: number )=> {
+ //   character.hp += amount;
+//}
 
-const Strider: Character = {
-    name: "Strider",
-    hp: 50,
-    showHP() {
-        console.log(`Puntos de Vida ${ this.hp}`);
-    }
-}
+//const Strider: Character = {
+ //   name: "Strider",
+ //   hp: 50,
+ //   showHP() {
+ //       console.log(`Puntos de Vida ${ this.hp}`);
+ //   }
+//}
 
-healCharacter(Strider, 150)
+//healCharacter(Strider, 150)
 
-Strider.showHP();
+//Strider.showHP();
 
-const result = addNumbers(1, 2);
-const result2 = addNumbersArrow(1, 2);
-const MultiplyResult = Multiply(5);
+//const result = addNumbers(1, 2);
+//const result2 = addNumbersArrow(1, 2);
+//const MultiplyResult = Multiply(5);
 
-console.log ({ result, result2, MultiplyResult});
+//console.log ({ result, result2, MultiplyResult});
