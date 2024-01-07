89b22f49bfd286527ae8b070f78414dff50c247d
aldalvarado <aaldoandre@gmail.com>
2024-01-06 19:25:39 -0600

    Desestructuracion 2

--- src/topics/05-basic-destructuring.ts
+++ src/topics/05-basic-destructuring.ts
@@ -1,31 +1,35 @@
-interface AudioPlayer {
-    audioVolume: number,
-    songDuration: number, 
-    song: string,
-    details: Details,
-}
+//interface AudioPlayer {
+//    audioVolume: number,
+  //  songDuration: number, 
+    //song: string,
+    //details: Details,
+//}
 
-interface Details {
-    author: string,
-    year: number,
-}
+//interface Details {
+  //  author: string,
+    //year: number,
+//}
 
-const audioPlayer: AudioPlayer = {
-    audioVolume: 90,
-    songDuration: 36,
-    song: "Mess",
-    details: {
-        author: "Ed Sheeran",
-        year: 2015
-    }
-}
+//const audioPlayer: AudioPlayer = {
+  //  audioVolume: 90,
+    //songDuration: 36,
+    //song: "Mess",
+    //details: {
+      //  author: "Ed Sheeran",
+        //year: 2015
+    //}
+//}
 
-const song = "New Song";
+//const [, , p3 = "No hay personaje"]: string[] = ["Goku", "Vegeta"];
 
-const { song:anotherSong, songDuration:duration } = audioPlayer;
+//console.log("Personaje 3: ", p3 || "No hay personaje");
 
-const {author:author, year:year} = audioPlayer.details;
+//const song = "New Song";
 
-console.log("Song: ", anotherSong);
-console.log("Duration: ", duration);
-console.log("Author: ", author);
+//const { song:anotherSong, songDuration:duration } = audioPlayer;
+
+//const {author:author, year:year} = audioPlayer.details;
+
+//console.log("Song: ", anotherSong);
+//console.log("Duration: ", duration);
+//console.log("Author: ", author);
